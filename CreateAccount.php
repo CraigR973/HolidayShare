4e478f68b20e825d7b3cecc19c5400c14e14ceaf
Blair Ramsay <drb14171@strath.ac.uk>
2017-03-17 21:52:29 +0000

    Added create account (Minus password hashing)

--- CreateAccount.php
+++ CreateAccount.php
@@ -7,8 +7,98 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <link rel="icon" sizes="276x299" href="docs/logo.png">
         <link rel="apple-touch-icon" href="docs/logo.png">
+        <script>
+            function checkForm(){
+                var errs = "";
+                var enteredusername = document.forms["myform"]["newusername"];
+                var entereduserpassword = document.forms["myform"]["newpassword"];
+                var enteredname = document.forms["myform"]["newname"];
+                var enteredage = document.forms["myform"]["newage"];
+        
+                enteredusername.style.background = "white";
+                entereduserpassword.style.background = "white";
+                enteredname.style.background = "white";
+                enteredname.style.background = "white";
+                
+                if((enteredusername.value==null) || (enteredusername.value=="")){
+                    errs+="   * Username must not be empty\n";
+                    enteredusername.style.background = "pink";
+                }
+                
+                if((entereduserpassword.value==null) || (entereduserpassword.value=="")){
+                    errs+="   * Password must not be empty\n";
+                    entereduserpassword.style.background = "pink";
+                }
+                
+                if((enteredname.value==null) || (enteredname.value=="")){
+                    errs+="   * Name must not be empty\n";
+                    enteredname.style.background = "pink";
+                }
+                
+                if((enteredage.value==null) || (enteredage.value=="")){
+                    errs+="   * Age must not be empty\n";
+                    enteredage.style.background = "pink";
+                }
+                
+                if(errs!==""){
+                    alert(errs);
+                }
+                
+                return (errs=="");
+                
+            }
+            
+        </script>
     </head>
     <body>
-        <h1>Create Account Goes Here</h1> 
+        <h2>Create Account</h2>
+        
+            <form method="post" name="myform" onsubmit="return checkForm();">
+            
+                <p class="login">
+                Create Username <input type="text" name="newusername"><br>
+                Create Password <input type="text" name="newpassword"><br>
+                <input type="submit" value="Create Account" name="CreateAccount">
+                </p>
+            
+            </form>
+        
+        <?php
+        
+        //connect to database
+        
+        $servername = "devweb2016.cis.strath.ac.uk";
+        $username = "cs312r";
+        $password = "seK8Veihau7d";
+        $database = "cs312r";
+        $conn = new mysqli($servername, $username, $password, $database);
+        
+        if($conn ->connect_error){
+            die("Connection Failed : ".$conn->connect_error);
+        }
+        
+        //setup variables from$_POST
+        
+        $newusername = isset($_POST['newusername']) ? $conn-> real_escape_string($_POST['newusername']): "";
+        
+        //Do password hashing later
+        //$newpassword = isset($_POST['newpassword']) ? $conn-> real_escape_string(password_hash($_POST['newpassword'],PASSWORD_DEFAULT)): ""; //real escape string may cause issues
+        
+        $newpassword = isset($_POST['newpassword']) ? $conn-> real_escape_string($_POST['newpassword']): "";
+        
+        if(isset($_POST["CreateAccount"])){
+        $sql = "INSERT INTO `Members` ( `username`, `password`, `location`) VALUES  ('$newusername','$newpassword', 'null')";
+        if($conn->query($sql) === TRUE){
+            echo"<h2>Account Created!</h2>";
+        } else{
+            die("Error on insert ".$conn->error);
+        }     
+        }
+        
+        //Close connection
+        $conn ->close();
+        
+        ?>
+        
     </body>
 </html>
